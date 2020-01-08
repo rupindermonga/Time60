5b94e3d511555556c6beaae924b8ebc43165e701
Rupinder <rupindermonga@gmail.com>
2020-01-08 13:44:04 +0530

    with add functionality

--- Time60.py
+++ Time60.py
@@ -2,8 +2,18 @@ import numpy as gs
 
 class Time60(object):
     def __init__(self, hr, mint):
-        self.hr = int(hr +round(mint/60,0))
+        self.hr = hr + int(mint/60)
         self.mint = gs.mod(mint, 60)
     
     def __str__(self):
         return '{}:{}'.format(self.hr, self.mint)
+
+    __repr__ = __str__
+
+    def __add__(self,other):
+        return self.__class__(self.hr+ other.hr, self.mint+other.mint)
+
+    def __iadd__(self, other):
+        self.hr += other.hr +int((self.mint+other.mint)/60)
+        self.mint += other.mint - int((self.mint+other.mint)/60)*60
+        return self
